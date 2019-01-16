1e373f1ae5c8b83a9492736439265ccb5c502c3c
Johari <joharijumali@gmail.com>
2019-01-16 09:27:32 +0800

    remove week report

--- resources/database/migrations/2018_12_06_154709_update_profile_data.php
+++ resources/database/migrations/2018_12_06_154709_update_profile_data.php
@@ -17,21 +17,21 @@ class UpdateProfileData extends Migration
         //     $table->unsignedInteger('position_id')->after('corporate_id')->nullable();
         // });
         
-        Schema::table('project_report', function (Blueprint $table) {
-            $table->unsignedInteger('creator_id')->after('workflow_id')->nullable();
-            $table->unsignedInteger('week')->after('creator_id')->nullable();
-            $table->date('report_end')->after('report_date')->nullable();
-        });
+        // Schema::table('project_report', function (Blueprint $table) {
+        //     $table->unsignedInteger('creator_id')->after('workflow_id')->nullable();
+        //     $table->unsignedInteger('week')->after('creator_id')->nullable();
+        //     $table->date('report_end')->after('report_date')->nullable();
+        // });
         
         Schema::table('project_card', function (Blueprint $table) {
             $table->unsignedInteger('month')->after('creator_id')->nullable();
             $table->date('card_end')->after('card_date')->nullable();
         });
 
-        Schema::table('project_report_workflow', function (Blueprint $table) {
-            $table->renameColumn('user_id', 'profile_id');
-            $table->string('state')->after('profile_id')->nullable();
-        });
+        // Schema::table('project_report_workflow', function (Blueprint $table) {
+        //     $table->renameColumn('user_id', 'profile_id');
+        //     $table->string('state')->after('profile_id')->nullable();
+        // });
 
         Schema::table('project_card_workflow', function (Blueprint $table) {
             $table->string('state')->after('profile_id')->nullable();
