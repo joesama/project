d907ab019a3900a279e7c9db5df146725e8a8888
Johari <joharijumali@gmail.com>
2019-01-16 23:59:58 +0800

    shortcut

--- resources/views/components/shortcut.blade.php
+++ resources/views/components/shortcut.blade.php
@@ -5,14 +5,14 @@
         <li class="col-xs-3" data-content="{{ trans('joesama/project::manager.dashboard.overall') }}">
             <a class="shortcut-grid" href="{!! handles('joesama/entree::'.config('joesama/entree::entree.landing','home')) !!}">
                 <div class="icon-wrap icon-wrap-sm icon-circle bg-mint">
-                <i class="psi-home"></i>
+                <i class="pli-project "></i>
                 </div>
             </a>
         </li>
         <li class="col-xs-3" data-content="{{ trans('joesama/project::dashboard.portfolio.master') }}">
             <a class="shortcut-grid" href="{!! handles('joesama/project::dashboard/portfolio/master/'.request()->segment(4)) !!}"">
                 <div class="icon-wrap icon-wrap-sm icon-circle bg-warning">
-                <i class="pli-project"></i>
+                <i class="psi-bar-chart-2"></i>
                 </div>
             </a>
         </li>
