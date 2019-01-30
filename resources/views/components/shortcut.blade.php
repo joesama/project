b5d2ba4b62eca011284efa4e1fe22bebf4831a70
Johari <joharijumali@gmail.com>
2019-01-30 09:23:32 +0800

    enable edit project before apprival

--- resources/views/components/shortcut.blade.php
+++ resources/views/components/shortcut.blade.php
@@ -9,6 +9,7 @@
                 </div>
             </a>
         </li>
+        @if($profile->corporate_id == 1)
         <li class="col-xs-3" data-content="{{ trans('joesama/project::dashboard.portfolio.master') }}">
             <a class="shortcut-grid" href="{!! handles('joesama/project::dashboard/portfolio/master/'.request()->segment(4)) !!}"">
                 <div class="icon-wrap icon-wrap-sm icon-circle bg-warning">
@@ -16,6 +17,15 @@
                 </div>
             </a>
         </li>
+        @else
+        <li class="col-xs-3" data-content="{{ trans('joesama/project::dashboard.portfolio.group') }}">
+            <a class="shortcut-grid" href="{!! handles('joesama/project::dashboard/portfolio/group/'.request()->segment(4)) !!}"">
+                <div class="icon-wrap icon-wrap-sm icon-circle bg-warning">
+                <i class="psi-bar-chart-2"></i>
+                </div>
+            </a>
+        </li>
+        @endif
         <li class="col-xs-3" data-content="{{ trans('joesama/project::manager.project.list') }}">
             <a class="shortcut-grid" href="{!! handles('joesama/project::manager/project/list/'.request()->segment(4)) !!}">
                 <div class="icon-wrap icon-wrap-sm icon-circle bg-success">
--- resources/views/manager/project/part/info.blade.php
+++ resources/views/manager/project/part/info.blade.php
@@ -7,7 +7,7 @@
 
                     </div>
                     <div class="col-md-3 text-right" class="vertical">
-                        @if( ($project->active || !is_null(data_get($project,'approval.approved_by'))) && $isProjectManager && is_null($isReport))
+                        @if( (($project->active || !is_null(data_get($project,'approval.approved_by')))  && is_null($isReport)) || $isProjectManager )
                         <a class="btn btn-dark mar-btm pull-right" href="{{ handles('joesama/project::manager/project/form/'.request()->segment(4).'/'.request()->segment(5)) }}">
                             <i class="psi-file-edit icon-fw"></i>
                             {{ __('joesama/vuegrid::datagrid.buttons.edit') }}
