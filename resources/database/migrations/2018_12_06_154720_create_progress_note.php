ae0bfac0a502d01968538bfabf1ccce436fa9db8
Johari <joharijumali@gmail.com>
2019-02-10 02:02:21 +0800

    fix naming error

--- resources/database/migrations/2018_12_06_154720_create_progress_note.php
+++ resources/database/migrations/2018_12_06_154720_create_progress_note.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProgressNotes extends Migration
+class CreateProgressNote extends Migration
 {
     /**
      * Run the migrations.
