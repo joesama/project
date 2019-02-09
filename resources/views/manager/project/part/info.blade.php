17c0a8858d87574a8a6ee4b028325ee49411850d
Johari <joharijumali@gmail.com>
2019-02-10 01:54:29 +0800

    #30

--- resources/views/manager/project/part/info.blade.php
+++ resources/views/manager/project/part/info.blade.php
@@ -7,7 +7,7 @@
 
                     </div>
                     <div class="col-md-3 text-right" class="vertical">
-                        @if( data_get($project,'approval.state') == 'rejected' && $isProjectManager )
+                        @if( strtolower(data_get($project,'approval.state')) == 'rejected' && $isProjectManager )
                         <a class="btn btn-dark mar-btm pull-right" href="{{ handles('joesama/project::manager/project/form/'.request()->segment(4).'/'.request()->segment(5)) }}">
                             <i class="psi-file-edit icon-fw"></i>
                             {{ __('joesama/vuegrid::datagrid.buttons.edit') }}
