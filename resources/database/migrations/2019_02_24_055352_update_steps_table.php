d491441749fc6175431a96c1c61ba2f4d353c878
Johari <joharijumali@gmail.com>
2019-03-10 00:22:50 +0800

    checking migration

--- resources/database/migrations/2019_02_24_055352_update_steps_table.php
+++ resources/database/migrations/2019_02_24_055352_update_steps_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateStepTable extends Migration
+class UpdateStepsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class UpdateStepTable extends Migration
      */
     public function down()
     {
-        Schema::drop('process_step');
+
     }
 }
